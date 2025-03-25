2b273fece76e14b38db1d4656128ac1933e8a94e
Suryaprakash Singh <surya.singh@posigen.com>
2025-03-25 18:53:38 +0000

    03/25 - Commits

--- sun/dev-pipeline/modularised/utils/.ipynb_checkpoints/logger-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-def setup_logger():
-    """Set up a logger for the pipeline run."""
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    
-    logging.getLogger("snowflake.connector").setLevel(logging.WARNING)
-    logging.getLogger("urllib3").setLevel(logging.WARNING)
-    return logging.getLogger(__name__)
